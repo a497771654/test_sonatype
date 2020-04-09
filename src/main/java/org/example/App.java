446144af8481062238ea3d705da32fc80d62dec7
admin <a497771654@gmail.com>
2020-04-09 14:40:08 +0800

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    /**
+     * 测试demo
+     * @param b 士大夫
+     * @return 123
+     */
+    public String a(String b){
+        return "123";
+    }
+}
